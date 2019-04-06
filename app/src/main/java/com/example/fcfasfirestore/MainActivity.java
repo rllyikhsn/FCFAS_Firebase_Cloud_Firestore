8203a93855bbbd043e4182ac824f1431eb391f9f
rllyikhsn <angledark68@gmail.com>
2019-04-06 17:22:11 +0700

    Operation Database Firebase - Cloud Firestore

---
+++ app/src/main/java/com/example/fcfasfirestore/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.fcfasfirestore;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends base implements View.OnClickListener {
+
+    private long mBackPressed;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_data_manage).setOnClickListener(this);
+        findViewById(R.id.btn_data_query).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_data_manage:
+                startActivity(new Intent(this, ManageActivity.class));
+                break;
+            case R.id.btn_data_query:
+                startActivity(new Intent(this, Query.class));
+                break;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mBackPressed + 2000 > System.currentTimeMillis()) {
+            super.onBackPressed();
+        } else {
+            Toast.makeText(this, R.string.exit, Toast.LENGTH_SHORT).show();
+            mBackPressed = System.currentTimeMillis();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/fcfasfirestore/MyDialog.java
@@ -0,0 +1,28 @@
+package com.example.fcfasfirestore;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+
+public class MyDialog {
+    private static Dialog mDialog;
+
+    public static void showDialog(Context context) {
+        mDialog = new Dialog(context, R.style.CustomProgressDialog);
+        mDialog.addContentView(
+                new ProgressBar(context),
+                new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        );
+        mDialog.setCancelable(true);
+        if (!mDialog.isShowing()) {
+            mDialog.show();
+        }
+    }
+
+    public static void dismissDialog() {
+        if (mDialog != null && mDialog.isShowing()) {
+            mDialog.dismiss();
+        }
+    }
+}
